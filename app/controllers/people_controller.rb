d6defd8648725b0c52d086ad33dbc20b29cdbe0c
qubitam <qubitam@gmail.com>
2022-08-21 02:22:50 +0100

    mostly covered testing people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,43 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: %i[ show edit update destroy ]
+
   def index
+    @people = Person.all
   end
+
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @person = Person.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @person = Person.new(person_params)
+
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to person_url(@person), notice: "Person was successfully created." }
+        format.json { render :show, status: :created, location: @person }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_person
+      @person = Person.find(params[:id])
+    end
+
+    def person_params
+      params.require(:person).permit(:first_name, :last_name, :email, :phone_number, :nickname)
+    end
 end
--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -1,10 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe "People", type: :request do
+
+  let(:valid_attributes) do
+    {
+      'id' => '1',
+      'first_name' => 'Joe',
+      'last_name' => 'Do',
+      'email' => 'email@app.com',
+      'phone_number' => '3333333333',
+      'nickname' => 'fast'
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      'id' => 'a',
+      'first_name' => 'Joe',
+      'last_name' => 'Do',
+      'email' => 'email@app.com',
+      'nickname' => 'fast'
+    }
+  end
+
   describe "GET /index" do
     it "returns http success" do
       get "/people/index"
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      person = Person.new(valid_attributes)
+      person.save
+      get person_url(person)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /new' do
+    it 'renders a successful response' do
+      get new_person_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it "creates a new Post" do
+        expect {
+          post people_url, params: { person: valid_attributes }
+        }.to change(Person, :count).by(1)
+      end
+
+      it "redirects to the created post" do
+        post people_url, params: { person: valid_attributes }
+        expect(response).to redirect_to(person_url(Person.last))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Person' do
+        expect {
+          post people_url, params: { person: invalid_attributes }
+        }.to change(Person, :count).by(0)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)" do
+        post people_url, params: { person: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 
